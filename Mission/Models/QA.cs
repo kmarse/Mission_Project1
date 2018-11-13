9c0680ba6c99e68dc169b8b1225ced4078ce307c
Kenneth Marse <marse.d.kenneth@gmail.com>
2018-11-12 21:20:39 -0700

    FAQ Fix

---
+++ Mission/Models/QA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mission.Models
+{
+    public class QA
+    {
+        [Key]
+        public int QuestionID { get; set; }
+        public string Mission { get; set; }
+        [Required(ErrorMessage = "Please enter your question")]
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
